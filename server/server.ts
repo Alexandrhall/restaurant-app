197410b7ed2fc5f5bab4970038dfb10ad8f5d37d
Alex <alexandrhall@gmail.com>
2022-08-23 21:12:44 +0200

    connectdb

--- server/server.ts
+++ server/server.ts
@@ -5,17 +5,29 @@ import cors from "cors";
 import mongoose, { ConnectOptions } from "mongoose";
 import mongodb, { MongoClient, ObjectId } from "mongodb";
 
-const client: MongoClient = new MongoClient(
-  "mongodb://localhost:27017/?readPreference=primary"
-);
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(
+      "mongodb://localhost:27017/?readPreference=primary"
+    );
+    console.log("Database is connected");
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
 
-const client2 = mongoose.connect(
-  "mongodb://localhost:27017/?readPreference=primary"
-);
+// const client: MongoClient = new MongoClient(
+//   "mongodb://localhost:27017/?readPreference=primary"
+// );
+
+// const client2 = mongoose.connect(
+//   "mongodb://localhost:27017/?readPreference=primary"
+// );
 
 const app: Express = express();
 
-mongoose.createConnection();
+// mongoose.createConnection();
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +36,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  connectDB();
   await TestModel.create({
     name: "Test Testerson",
   });
@@ -42,17 +53,6 @@ app.post("/createbook", async (req, res) => {
 app.listen(8000, () => {
   console.log("Live at http://localhost:8000");
 });
-
-const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(
-      "mongodb://localhost:27017/?readPreference=primary"
-    );
-    console.log("Database is connected");
-  } catch (error: any) {
-    console.log(error.message);
-  }
-};
 
 interface Table {
   _id: ObjectId;
