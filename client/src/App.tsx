4c3df4383d41938a816f9e4c0c7011cc164c10a8
Alexandrhall <alexandrhall@gmail.com>
2022-08-24 13:44:06 +0200

    calendar in and schemas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
   const [value, onChange] = useState(new Date());
