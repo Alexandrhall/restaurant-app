38397f13143ccc6815278d3505d24c7949a5e694
PeppZZon <jespersimonlind@gmail.com>
2022-08-24 14:06:13 +0200

    changed name

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,7 +1,7 @@
-import React from "React";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export const Nav = () => {
+export const Navigation = () => {
   return (
     <div className="navWrapper">
       <div className="logoWrapper">
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Navigation } from "../Navigation";
 
 export const Home = () => {
-  return <></>;
+  return (
+    <header>
+      <Navigation></Navigation>
+    </header>
+  );
 };
