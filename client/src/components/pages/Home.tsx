3d7c4043dd0c89cc0ab129ef39acb43e40c06ad3
PeppZZon <jespersimonlind@gmail.com>
2022-08-26 10:54:42 +0200

    added html

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -5,6 +5,12 @@ export const Home = () => {
   return (
     <header>
       <Navigation></Navigation>
+      <div className="hero-image">
+        <div className="hero-text">
+          Classic Food With The Taste of Homecooked
+        </div>
+        <p className="book-button">Book a table</p>
+      </div>
     </header>
   );
 };
