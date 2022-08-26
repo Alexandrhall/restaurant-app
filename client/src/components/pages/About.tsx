c6f52fb8fcfa3bba0273fc1d05bf4d174a48eb99
PeppZZon <jespersimonlind@gmail.com>
2022-08-26 14:56:55 +0200

    Changed structure and color

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -2,29 +2,24 @@ import React from "react";
 import "../../styles/about.scss";
 export const About = () => {
   return (
-    <div className="wrapper">
-      <div className="titleText">
-        <h2>About Us</h2>
-      </div>
-      <div className="img-container">
-        <div className="image"></div>
-      </div>
-      <div className="aboutText">
-        <p>
+    <div className="box">
+      <div className="text">
+        <p className="title-text">About Us</p>
+        <p className="info-text">
           Since our modest beginnings in 2005 with a little space in Toronto’s
           stylish Yorkville locale, ‘Organization Name’ ‘s development has been
           enlivened with the energy to cook and serve solid, Indian-roused
-          takeout food. <br></br>In contrast to other Indian eateries,
-          ‘Organization Name’ was made with the explicit expectation to appear
-          as something else. <br></br> We realize numerous individuals love
-          Indian sustenance, yet a large number of them loathe or are
-          unconscious of the regularly unfortunate fixings that make
-          run-of-the-mill Indian nourishment taste so great. <br></br> Our menu
-          highlights things that utilization the sound and fragrant flavors,
-          however, forgets the stuffing ghee, spread, oil, and overwhelming
-          cream.
+          takeout food. In contrast to other Indian eateries, ‘Organization
+          Name’ was made with the explicit expectation to appear as something
+          else. We realize numerous individuals love Indian sustenance, yet a
+          large number of them loathe or are unconscious of the regularly
+          unfortunate fixings that make run-of-the-mill Indian nourishment taste
+          so great. Our menu highlights things that utilization the sound and
+          fragrant flavors, however, forgets the stuffing ghee, spread, oil, and
+          overwhelming cream.
         </p>
       </div>
+      <div className="image-container"></div>
     </div>
   );
 };
