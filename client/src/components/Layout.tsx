0563141465b290a20b50c678e73280811161d9d0
Alex <alexandrhall@gmail.com>
2022-08-26 15:40:44 +0200

    navbar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <div className="App">
       <h1>Restaurant booking</h1>
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/booking"}>Booking</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </nav>
       <Outlet></Outlet>
     </div>
   );
