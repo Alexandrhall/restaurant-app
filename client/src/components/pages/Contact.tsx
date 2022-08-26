55fa82ce970d353cb442780e1bcf316e577520fb
PeppZZon <jespersimonlind@gmail.com>
2022-08-26 15:12:08 +0200

    Added form html

--- client/src/components/pages/Contact.tsx
+++ client/src/components/pages/Contact.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Navigation } from "../Navigation";
 
 export const Contact = () => {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Navigation></Navigation>
+      <div className="contact">
+        <p className="contact-text">Contact</p>
+        <div className="contact-form">
+          <form action="" method="post">
+            <input type="text" name="name" placeholder="Your name..." />
+            <input type="text" name="email" placeholder="Your email..." />
+            <textarea
+              name="message"
+              placeholder="Write something..."
+            ></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
