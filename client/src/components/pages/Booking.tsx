ad672819185e38f6e5486b8c4b39acdb871b8088
Alex <alexandrhall@gmail.com>
2022-08-27 13:52:10 +0200

    datepciker

--- client/src/components/pages/Booking.tsx
+++ client/src/components/pages/Booking.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export const Booking = () => {
   const [dateValue, setDateValue] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
 
-  // console.log(dateValue);
   return (
     <>
       <h1>Booking page</h1>
       <Calendar onChange={setDateValue} value={dateValue} />
       <p>{dateValue.toString()}</p>
+      <DatePicker
+        selected={dateValue}
+        onChange={(date: Date) => setDateValue(date)}
+        showTimeSelect
+      />
 
       <form action="http://localhost:8000/booking" method="post">
         <input type="text" name="name" placeholder="First Name" />
         <input type="submit" value="submit" />
-        {/* <input type="date" value={dateValue.toString("yyyy-MM-dd")} /> */}
       </form>
     </>
   );
