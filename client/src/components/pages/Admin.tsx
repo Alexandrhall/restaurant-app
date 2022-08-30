0c00a4151a789988a0c11b9d1a7b81d1173f74b3
PeppZZon <jespersimonlind@gmail.com>
2022-08-30 18:34:41 +0200

    Added background image

--- client/src/components/pages/Admin.tsx
+++ client/src/components/pages/Admin.tsx
@@ -1,19 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { IBookings } from "../../../../server/models/IBookings";
+import "../../styles/admin.scss";
 
 export const Admin = () => {
-  const [bookings, setBookings] = useState<IBookings[]>([]);
+  // Kanske kan använda det här sen också
+  // const [bookings, setBookings] = useState<IBookings[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/admin/")
-      .then((response) => response.json())
-      .then((data) => setBookings(data));
-  });
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/admin/")
+  //     .then((response) => response.json())
+  //     .then((data) => setBookings(data));
+  // });
   return (
-    <div className="listWrapper">
-      {bookings.map((booking) => {
-        return <div>{booking.time}</div>;
-      })}
+    <div className="admin-image">
+      <div className="admin-login-cnt">
+        <span className="admin-login-user">Admin</span>
+        <div className="admin-login-card">
+          <form action="/admin" className="admin-login-form">
+            <input
+              className="admin-login-input"
+              type="text"
+              name="username"
+              placeholder="Användarnamn"
+            ></input>
+            <input
+              className="admin-login-input"
+              type="password"
+              name="password"
+              placeholder="Lösenord"
+            ></input>
+            <input
+              type="submit"
+              value="Logga in"
+              className="admin-login-btn"
+            ></input>
+          </form>
+        </div>
+      </div>
     </div>
+
+    // Kanske kan använda det här sen
+    // <div className="listWrapper">
+    //   {bookings.map((booking) => {
+    //     return <div>{booking.time}</div>;
+    //   })}
+    // </div>
   );
 };
