d2043a27fb68bf15ee238b01d683595e77be26fe
Alex <alexandrhall@gmail.com>
2022-08-30 15:26:39 +0200

    testing server

--- client/src/components/pages/Booking.tsx
+++ client/src/components/pages/Booking.tsx
@@ -21,9 +21,9 @@ export const Booking = () => {
         <DatePicker
           selected={dateValue}
           onChange={(date: Date) => setDateValue(date)}
-          showTimeSelect
+          // showTimeSelect
           showWeekNumbers
-          timeIntervals={180}
+          //timeIntervals={180}
           calendarStartDay={1}
           timeFormat="HH:mm"
           popperPlacement="bottom"
@@ -38,10 +38,17 @@ export const Booking = () => {
           minTime={setHours(setMinutes(new Date(), 0), 18)}
           maxTime={setHours(setMinutes(new Date(), 0), 21)}
         />
+        {/* <input type="date" name="date2" /> */}
         <input type="text" name="name" placeholder="First Name" />
         <input type="number" name="phone" placeholder="Phone number" />
         <input type="email" name="email" placeholder="Email" />
-        <input type="number" name="seats" placeholder="Seats" max={6} min={1} />
+        <input
+          type="number"
+          name="persons"
+          placeholder="Persons"
+          max={6}
+          min={1}
+        />
         <input type="submit" value="Submit" />
       </form>
     </>
