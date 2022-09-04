4a7ccd7a99edcb481d9c7296cb75333d51029bfc
PeppZZon <jespersimonlind@gmail.com>
2022-09-04 16:43:36 +0200

    Very basic styling

--- client/src/components/pages/Adminbookings.tsx
+++ client/src/components/pages/Adminbookings.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IBookings } from "../../models/IBookings";
 import { Booking } from "./Booking";
+import "../../styles/adminbookings.scss";
 
 export const Adminbookings = () => {
   const [bookings, setBookings] = useState<IBookings[]>([]);
@@ -12,32 +13,34 @@ export const Adminbookings = () => {
       .then((data) => setBookings(data));
   }, []);
   return (
-    <div className="bookings-container">
-      <div className="bookings-card">
-        <div className="text-container">
-          <p>Date</p>
-          <p>Name: & E-mail</p>
-          <p>Amount</p>
-          <p>Time</p>
-          <p>Phone</p>
-        </div>
-        <div className="listWrapper">
-          {bookings.map((booking) => {
-            return (
-              <div>
-                <p>{booking.date}</p>
-                <p>
-                  {booking.information.name} {booking.information.email}
-                </p>
-                <p>{booking.persons}</p>
-                <p>{booking.time}</p>
-                <p>{booking.information.phone}</p>
-                <div className="editBtn">
-                  <Link to={"/admin/bookings" + booking._id}>Edit</Link>
+    <div className="background">
+      <div className="bookings-container">
+        <div className="bookings-card">
+          <div className="text-container">
+            <p className="text-title">Date</p>
+            <p className="text-title">Name: & E-mail</p>
+            <p className="text-title">Amount</p>
+            <p className="text-title">Time</p>
+            <p className="text-title">Phone</p>
+          </div>
+          <div className="listWrapper">
+            {bookings.map((booking) => {
+              return (
+                <div className="single-booking">
+                  <p className="booking-info">{booking.date}</p>
+                  <p className="booking-info">
+                    {booking.information.name} {booking.information.email}
+                  </p>
+                  <p className="booking-info">{booking.persons}</p>
+                  <p className="booking-info">{booking.time}</p>
+                  <p className="booking-info">{booking.information.phone}</p>
+                  <div className="editBtn">
+                    <Link to={"/admin/bookings" + booking._id}>Edit</Link>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
