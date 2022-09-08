3fd05b3c58a691f27cfafa4fc960e727a37c9b51
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 15:26:40 +0200

    Redesigned Contact

--- client/src/components/pages/Contact.tsx
+++ client/src/components/pages/Contact.tsx
@@ -3,52 +3,33 @@ import "../../styles/contact.scss";
 
 export const Contact = () => {
   return (
-    <div className="wrapper">
-      {/* <Navigation></Navigation> */}
-      <div className="contact">
-        <p className="contact-text">Contact</p>
-        <div className="image-box">
-          <img
-            className="contactImage"
-            src="https://www.homedoo.com/wp-content/uploads/2016/04/AK-A_48_Urban_Garden_32.jpg"
-            alt="our restaurant"
-          />
-        </div>
-        <h2 className="contact-mobile-text"> Send us a mail</h2>
-        <div className="contact-form">
-          <form className="con-form" action="" method="post">
+    <div className="background">
+      <div className="contact-cnt">
+        <span className="contact-title">Contact Us</span>
+        <div className="contact-card">
+          <form action="" className="contact-form">
+            <label htmlFor="name">Name</label>
             <input
               className="contact-input"
               type="text"
               name="name"
-              placeholder="Your name..."
+              placeholder="Your Name..."
             />
+            <label htmlFor="email">Email</label>
             <input
               className="contact-input"
               type="text"
               name="email"
-              placeholder="Your email..."
+              placeholder="Your Email..."
             />
+            <label htmlFor="message">Message</label>
             <textarea
               className="contact-input"
               name="message"
-              placeholder="Write something..."
-            ></textarea>
-            <input className="contact-submit" type="submit" value="Send" />
+              placeholder="Your Message..."
+            />
+            <input type="submit" value="Send" className="contact-send"></input>
           </form>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="footer-container">
-          <p>
-            Phone <br></br> 070-865-70-08
-          </p>
-          <p>
-            Email <br></br> restaurant@gmail.com
-          </p>
-          <p>
-            Adress <br></br> Gustavlundsvägen 151D <br></br> 168 80 Bromma
-          </p>
         </div>
       </div>
     </div>
