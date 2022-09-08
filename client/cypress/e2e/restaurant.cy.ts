2abe0fa90886c809c5d57471786c006d2bb8a363
Alex <alexandrhall@gmail.com>
2022-09-09 00:37:01 +0200

    cancel mail

--- client/cypress/e2e/restaurant.cy.ts
+++ client/cypress/e2e/restaurant.cy.ts
@@ -8,6 +8,16 @@ describe("Restaurant testing", () => {
 
     cy.get("p").contains("is incorrect");
   });
+  it("it should click to admin page and see booking info", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get(".login-button").click();
+
+    cy.get(".admin-login-btn").click();
+  });
+  it("it should", () => {});
+  it("it should", () => {});
+  it("it should", () => {});
 });
 
 export {};
--- server/server.ts
+++ server/server.ts
@@ -32,7 +32,7 @@ app.get("/", async (req, res) => {
 });
 
 const contactEmail = nodemailer.createTransport({
-  service: "gmaill",
+  service: "gmail",
   auth: authInfo,
 });
 
@@ -52,6 +52,33 @@ app.get("/booking/:id", async (req, res) => {
 
 app.delete("/booking/delete/:id", async (req, res) => {
   const del = await BookModel.findByIdAndDelete(req.params.id);
+
+  const mail = {
+    from: "Restaurant 10",
+    to: del?.information.email,
+    subject: "Canceled booking",
+    html: `<h1> Hello ${del?.information.name}</h1>
+    <p>You have canceled your reservation</p>
+
+    <p>Your booking information:</p>
+    <p>Name: ${del?.information.name}</p>
+    <p>Phone: +46${del?.information.phone}</p>
+    <p>Email: ${del?.information.email}</p>
+    <p>Persons: ${del?.persons}</p>
+    <p>Date: ${del?.date}</p>
+    <p>Time: ${del?.time}</p>
+
+    <p> If you have any questions please contact us on the information below.</p>
+    <p> Phone: 070-865-70-08 Email: group10restaurant@gmail.com</p>`,
+  };
+
+  contactEmail.sendMail(mail, (err) => {
+    if (err) {
+      res.json({ status: "Error" });
+    } else {
+      res.json({ status: "Sent" });
+    }
+  });
 
   res.send(del);
 });
