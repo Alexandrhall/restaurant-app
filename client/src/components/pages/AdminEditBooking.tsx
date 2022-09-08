1a0196ffbd03df7f6d590a43e8dd5ce230473e39
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 09:44:13 +0200

    Stuff

--- client/src/components/pages/AdminEditBooking.tsx
+++ client/src/components/pages/AdminEditBooking.tsx
@@ -15,55 +15,57 @@ export const AdminEditBooking = () => {
       .then((data) => setSingleBooking(data));
   }, []);
   return (
-    <form>
-      <h2>Edit booking for {singleBooking?.information.name}</h2>
+    <div className="wrapper">
+      <form>
+        <h2>Edit booking for {singleBooking?.information.name}</h2>
 
-      <input
-        className="Something"
-        type="number"
-        name="persons"
-        value={singleBooking?.persons}
-      ></input>
+        <input
+          className="Something"
+          type="number"
+          name="persons"
+          value={singleBooking?.persons}
+        ></input>
 
-      <select className="Something" name="time" value={singleBooking?.time}>
-        <option value="" disabled selected>
-          Select time
-        </option>
-        <option value="18">18</option>
-        <option value="21">21</option>
-      </select>
+        <select className="Something" name="time" value={singleBooking?.time}>
+          <option value="" disabled selected>
+            Select time
+          </option>
+          <option value="18">18</option>
+          <option value="21">21</option>
+        </select>
 
-      <input
-        className="Something"
-        type="text"
-        name="date"
-        value={singleBooking?.date}
-      ></input>
+        <input
+          className="Something"
+          type="text"
+          name="date"
+          value={singleBooking?.date}
+        ></input>
 
-      <h3>Edit personal information for {singleBooking?.information.name}</h3>
+        <h3>Edit personal information for {singleBooking?.information.name}</h3>
 
-      <input
-        className="Something"
-        type="text"
-        name="name"
-        value={singleBooking?.information.name}
-      ></input>
+        <input
+          className="Something"
+          type="text"
+          name="name"
+          value={singleBooking?.information.name}
+        ></input>
 
-      <input
-        className="Something"
-        type="text"
-        name="email"
-        value={singleBooking?.information.email}
-      ></input>
+        <input
+          className="Something"
+          type="text"
+          name="email"
+          value={singleBooking?.information.email}
+        ></input>
 
-      <input
-        className="Something"
-        type="number"
-        name="phone"
-        value={singleBooking?.information.phone}
-      ></input>
-      <button className="Something">Back</button>
-      <button className="Something">Save</button>
-    </form>
+        <input
+          className="Something"
+          type="number"
+          name="phone"
+          value={singleBooking?.information.phone}
+        ></input>
+        <button className="Something">Back</button>
+        <button className="Something">Save</button>
+      </form>
+    </div>
   );
 };
