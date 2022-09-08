818dfaeac8712ecea7f5433c6473547281e2e5fe
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 15:57:09 +0200

    Redesigned About

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -2,31 +2,42 @@ import React from "react";
 import "../../styles/about.scss";
 export const About = () => {
   return (
-    <div className="box">
-      {/* <Navigation></Navigation> */}
-      <div className="image-container">
-        <img
-          className="aboutImage"
-          src="https://usercontent.one/wp/www.mangalturkiskkolgrill.se/wp-content/uploads/2021/01/volkan-vardar-1H30uRC1plc-unsplash.jpg?media=1631447692"
-          alt="our restaurant"
-        />
-      </div>
-      <div className="text">
-        <p className="title-text">About Us</p>
-        <p className="info-text">
-          Since our modest beginnings in 2005 with a little space in Toronto’s
-          stylish Yorkville locale, ‘Organization Name’ ‘s development has been
-          enlivened with the energy to cook and serve solid, Indian-roused
-          takeout food. <br></br> In contrast to other Indian eateries,
-          ‘Organization Name’ was made with the explicit expectation to appear
-          as something else. <br></br> We realize numerous individuals love
-          Indian sustenance, yet a large number of them loathe or are
-          unconscious of the regularly unfortunate fixings that make
-          run-of-the-mill Indian nourishment taste so great. <br></br> Our menu
-          highlights things that utilization the sound and fragrant flavors,
-          however, forgets the stuffing ghee, spread, oil, and overwhelming
-          cream.
-        </p>
+    <div className="background">
+      <div className="about-cnt">
+        <span className="about-title">About Us</span>
+        <div className="about-card">
+          <div className="about-text">
+            <p>
+              Since our modest beginnings in 2005 with a little space in
+              Toronto’s stylish Yorkville locale, ‘Organization Name’ ‘s
+              development has been enlivened with the energy to cook and serve
+              solid, Indian-roused takeout food.
+            </p>
+            <p>
+              In contrast to other Indian eateries, ‘Organization Name’ was made
+              with the explicit expectation to appear as something else.
+            </p>
+            <p>
+              We realize numerous individuals love Indian sustenance, yet a
+              large number of them loathe or are unconscious of the regularly
+              unfortunate fixings that make run-of-the-mill Indian nourishment
+              taste so great.
+            </p>
+            <p>
+              Our menu highlights things that utilization the sound and fragrant
+              flavors, however, forgets the stuffing ghee, spread, oil, and
+              overwhelming cream.
+            </p>
+            <hr></hr>
+          </div>
+          <div className="opening-hours">
+            <p>Opening Hours</p>
+            <p>Monday - Thursday 17:00 - 00:00 </p>
+            <p>Friday 17:00 - 01:00 </p>
+            <p>Saturday 13:00 - 01:00 </p>
+            <p>Sunday 17:00 - 00:00 </p>
+          </div>
+        </div>
       </div>
     </div>
   );
