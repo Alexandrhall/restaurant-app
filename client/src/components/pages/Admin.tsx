091fc8ef73f1be52a6e5ee7a1215ea3b0451723f
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 23:40:20 +0200

    Added link

--- client/src/components/pages/Admin.tsx
+++ client/src/components/pages/Admin.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { IBookings } from "../../models/IBookings";
 import "../../styles/admin.scss";
 
