49e98902fe8fcfa967b4e7895fc7544fd35bbff8
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 14:01:03 +0200

    All Bookings Mobile Styling

--- client/src/components/pages/Adminbookings.tsx
+++ client/src/components/pages/Adminbookings.tsx
@@ -16,24 +16,29 @@ export const Adminbookings = () => {
     <div className="background">
       <div className="bookings-container">
         <div className="bookings-card">
-          <div className="text-container">
+          {/* <div className="text-container">
             <p className="text-title">Date</p>
             <p className="text-title">Name: & E-mail</p>
             <p className="text-title">Amount</p>
             <p className="text-title">Time</p>
             <p className="text-title">Phone</p>
-          </div>
+          </div> */}
           <div className="listWrapper">
             {bookings.map((booking) => {
               return (
                 <div className="single-booking">
+                  <p className="booking-mobile-text">Date:</p>
                   <p className="booking-info">{booking.date}</p>
                   <p className="booking-info">
+                    <p className="booking-mobile-text">Name & Email:</p>
                     {booking.information.name} <br></br>
                     {booking.information.email}
                   </p>
+                  <p className="booking-mobile-text">Persons:</p>
                   <p className="booking-info">{booking.persons}</p>
+                  <p className="booking-mobile-text">Time:</p>
                   <p className="booking-info">{booking.time}</p>
+                  <p className="booking-mobile-text">Phone:</p>
                   <p className="booking-info">{booking.information.phone}</p>
                   <div className="editBtn-cnt">
                     <Link
