9ba9f449e5cb86e00818c58228f89afa5f61b0ef
PeppZZon <jespersimonlind@gmail.com>
2022-09-08 17:28:23 +0200

    Styled edit booking, desktop and mobile

--- client/src/components/pages/AdminEditBooking.tsx
+++ client/src/components/pages/AdminEditBooking.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { IBookings } from "../../models/IBookings";
 import "../../styles/editbooking.scss";
 export const AdminEditBooking = () => {
@@ -15,57 +15,73 @@ export const AdminEditBooking = () => {
       .then((data) => setSingleBooking(data));
   }, []);
   return (
-    <div className="wrapper">
-      <form>
-        <h2>Edit booking for {singleBooking?.information.name}</h2>
+    <div className="background">
+      <div className="edit-booking-cnt">
+        <div className="edit-booking-card">
+          <span className="edit-title">
+            Edit booking for {singleBooking?.information.name}
+          </span>
+          <form className="edit-booking-form">
+            <label htmlFor="persons">Number of persons</label>
+            <input
+              className="booking-info-edit"
+              type="number"
+              name="persons"
+              value={singleBooking?.persons}
+            ></input>
+            <label htmlFor="time">Time</label>
+            <select
+              className="booking-info-edit"
+              name="time"
+              value={singleBooking?.time}
+            >
+              <option value="" disabled selected>
+                Select time
+              </option>
+              <option value="18">18</option>
+              <option value="21">21</option>
+            </select>
+            <label htmlFor="date">Date</label>
+            <input
+              className="booking-info-edit"
+              type="text"
+              name="date"
+              value={singleBooking?.date}
+            ></input>
 
-        <input
-          className="Something"
-          type="number"
-          name="persons"
-          value={singleBooking?.persons}
-        ></input>
-
-        <select className="Something" name="time" value={singleBooking?.time}>
-          <option value="" disabled selected>
-            Select time
-          </option>
-          <option value="18">18</option>
-          <option value="21">21</option>
-        </select>
-
-        <input
-          className="Something"
-          type="text"
-          name="date"
-          value={singleBooking?.date}
-        ></input>
-
-        <h3>Edit personal information for {singleBooking?.information.name}</h3>
-
-        <input
-          className="Something"
-          type="text"
-          name="name"
-          value={singleBooking?.information.name}
-        ></input>
-
-        <input
-          className="Something"
-          type="text"
-          name="email"
-          value={singleBooking?.information.email}
-        ></input>
-
-        <input
-          className="Something"
-          type="number"
-          name="phone"
-          value={singleBooking?.information.phone}
-        ></input>
-        <button className="Something">Back</button>
-        <button className="Something">Save</button>
-      </form>
+            <span className="edit-title-light">Edit personal information</span>
+            <label htmlFor="name">Name</label>
+            <input
+              className="booking-info-edit"
+              type="text"
+              name="name"
+              value={singleBooking?.information.name}
+            ></input>
+            <label htmlFor="email">Email</label>
+            <input
+              className="booking-info-edit"
+              type="text"
+              name="email"
+              value={singleBooking?.information.email}
+            ></input>
+            <label htmlFor="number">Number</label>
+            <input
+              className="booking-info-edit"
+              type="number"
+              name="phone"
+              value={singleBooking?.information.phone}
+            ></input>
+            <div className="button-container">
+              <Link className="back-btn" to={"/admin/bookings"}>
+                Back
+              </Link>
+              <Link className="save-btn" to={"/admin/bookings"}>
+                Save
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
