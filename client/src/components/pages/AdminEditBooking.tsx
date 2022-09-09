a02513daf87f9c689c7dc4b2ac88e7011567c8bf
PeppZZon <jespersimonlind@gmail.com>
2022-09-09 08:48:50 +0200

    Sttuff

--- client/src/components/pages/AdminEditBooking.tsx
+++ client/src/components/pages/AdminEditBooking.tsx
@@ -3,8 +3,27 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { IBookings } from "../../models/IBookings";
 import "../../styles/editbooking.scss";
+
+export interface something {
+  name: string;
+  persons: number;
+  date: string;
+  time: string;
+  email: string;
+  phone: string;
+}
 export const AdminEditBooking = () => {
   const [singleBooking, setSingleBooking] = useState<IBookings>();
+
+  const initialstate = {
+    name: "",
+    persons: 0,
+    date: "",
+    time: "",
+    email: "",
+    phone: "",
+  };
+  const [valState, setValState] = useState<something>(initialstate);
 
   const params = useParams();
   const { id } = useParams();
@@ -12,9 +31,9 @@ export const AdminEditBooking = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:8000/admin/bookings/" + id)
-      .then((response) => response.json())
-      .then((data) => setSingleBooking(data));
+    axios.get("http://localhost:8000/admin/bookings/" + id).then((resp) => {
+      // valState(resp.data);
+    });
   }, []);
 
   const editedBooking = () => {
@@ -26,26 +45,32 @@ export const AdminEditBooking = () => {
       });
   };
 
+  const valOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div className="background">
       <div className="edit-booking-cnt">
         <div className="edit-booking-card">
-          <span className="edit-title">
-            Edit booking for {singleBooking?.information.name}
-          </span>
+          <span className="edit-title">Edit booking for {valState.name}</span>
           <form className="edit-booking-form">
             <label htmlFor="persons">Number of persons</label>
             <input
               className="booking-info-edit"
               type="number"
               name="persons"
-              value={singleBooking?.persons}
+              value={valState.persons}
+              onChange={valOnChange}
             ></input>
             <label htmlFor="time">Time</label>
             <select
               className="booking-info-edit"
               name="time"
-              value={singleBooking?.time}
+              value={valState.time}
             >
               <option value="" disabled selected>
                 Select time
@@ -58,7 +83,7 @@ export const AdminEditBooking = () => {
               className="booking-info-edit"
               type="text"
               name="date"
-              value={singleBooking?.date}
+              value={valState.date}
             ></input>
 
             <span className="edit-title-light">Edit personal information</span>
@@ -67,21 +92,24 @@ export const AdminEditBooking = () => {
               className="booking-info-edit"
               type="text"
               name="name"
-              value={singleBooking?.information.name}
+              value={valState.name}
+              onChange={valOnChange}
             ></input>
             <label htmlFor="email">Email</label>
             <input
               className="booking-info-edit"
               type="text"
               name="email"
-              value={singleBooking?.information.email}
+              value={valState.email}
+              onChange={valOnChange}
             ></input>
             <label htmlFor="number">Number</label>
             <input
               className="booking-info-edit"
               type="number"
               name="phone"
-              value={singleBooking?.information.phone}
+              value={valState.phone}
+              onChange={valOnChange}
             ></input>
             <div className="button-container">
               <Link className="back-btn" to={"/admin/bookings"}>
